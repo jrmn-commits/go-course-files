28769c258d1e4a1981ce04a5352d2ec086958761
jrmn-commits <113871835+jrmn-commits@users.noreply.github.com>
2023-10-30 20:44:46 -0400

    Maps & Slices Activity

---
+++ maps_slices.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	//	"sort"
+)
+
+//	type User struct {
+//	firstName string
+//	lastName  string
+//	}
+
+func main() {
+	// SLICES
+	names := []string{"one", "seven", "fish", "cat"}
+
+	log.Println(names)
+	
+	// ARRAYS
+	//	numbers := []int{10, 11, 12, 13, 14}
+
+	//	var mySlice []string
+	//	var myInt []int
+
+	//	myInt = append(myInt, 3)
+	//	myInt = append(myInt, 1)
+	//	myInt = append(myInt, 2)
+
+	//	mySlice = append(mySlice, "Trevor")
+	//	mySlice = append(mySlice, "John")
+	//	mySlice = append(mySlice, "Mary")
+
+	//	sort.Ints(myInt)
+	//	log.Println(mySlice, myInt, numbers[0:3])
+
+	// MAPS
+
+	//	myMap := make(map[string]User)
+
+	//	me := User{
+	//	firstName: "John",
+	//	lastName:  "Doe",
+	//	}
+
+	//	myMap["me"] = me
+
+	//	log.Println(myMap["me"].firstName)
+
+	//	var myNewVar float32
+
+	//	myNewVar = 3.141
+}
